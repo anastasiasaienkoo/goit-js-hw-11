7ae8d88aa7372f8506e91501e2f112c190473dc4
Anastasia <n05012007@gmail.com>
2025-01-16 22:08:50 +0100

    up

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,7 +1,7 @@
-import "izitoast/dist/css/iziToast.min.css";
-import iziToast from "izitoast";
+// import "izitoast/dist/css/iziToast.min.css";
+// import iziToast from "izitoast";
 
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
+// import SimpleLightbox from "simplelightbox";
+// import "simplelightbox/dist/simple-lightbox.min.css";
 
-const keyApi = '47906574-737e3153d4e9e6f9b55134ae6';
+// const keyApi = '47906574-737e3153d4e9e6f9b55134ae6';
