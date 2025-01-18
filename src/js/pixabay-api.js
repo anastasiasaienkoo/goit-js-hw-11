9a287d79b73bf8b72e44de39a71358258fe380f1
Anastasia <n05012007@gmail.com>
2025-01-18 17:11:39 +0100

    update

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -22,32 +22,5 @@ export function fetchImages(value){
     }
     return response.json();
 })
-.then(data => {
-    if(data.hits.length === 0){
-        iziToast.show({
-            message: "Sorry, there are no images matching your search query. Please try again!",
-            iconUrl: 'https://symbl-world.akamaized.net/i/webp/c1/d9d88630432cf61ad335df98ce37d6.webp',
-            messageSize: '16',
-            messageColor: 'white',
-            backgroundColor: '#EF4040',
-            position: 'topRight',
-            width: '302',
-            color: 'white',
-        });
-       return;
-    }
-    
-   const markup = galleryFn(data.hits);
-    ul.innerHTML = markup;
-
-    const simplebox = new SimpleLightbox('.gallery a', {
-        captionsData: "alt", // встановлює підпис
-        captionDelay: 250, // встановлює затримку
-        doubleTapZoom: 0, // вбирає зум з подвійним кліком
-        maxZoom: 0, // вбирає зум
-        scrollZoom: false, //вбирає скрол мишою
-    });
-    simplebox.refresh()
-})}
-
+}
 
--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
 import "izitoast/dist/css/iziToast.min.css";
 import iziToast from "izitoast";
+import { galleryFn } from './js/render-funktions.js';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+import {fetchImages} from './js/pixabay-api.js';
 const form = document.querySelector('form');
 const input = form.querySelector('.input-form');
-import {fetchImages} from './js/pixabay-api.js';
 const loader = document.querySelector('.loader');
+const gallery =  document.querySelector('.gallery');
 let valueInput = '';
+const ul = document.querySelector('.gallery-js');
 
 
 form.addEventListener('submit', event => {
@@ -26,20 +31,46 @@ form.addEventListener('submit', event => {
     } 
         loader.classList.remove('is-hidden');
         fetchImages(valueInput)
-        // .catch(error => {
-        //     refs.loader.style.display = 'none';
-        //     iziToast.error({
-        //         message: 'Error fetching images. Please try again later.',
-        //         position: 'topRight',
-        //       });
-        //       console.error(error);
-        // })
+        .then(data => {
+            if(data.hits.length === 0){
+                iziToast.show({
+                    message: "Sorry, there are no images matching your search query. Please try again!",
+                    iconUrl: 'https://symbl-world.akamaized.net/i/webp/c1/d9d88630432cf61ad335df98ce37d6.webp',
+                    messageSize: '16',
+                    messageColor: 'white',
+                    backgroundColor: '#EF4040',
+                    position: 'topRight',
+                    width: '302',
+                    color: 'white',
+                });
+               return;
+            }
+            
+           const markup = galleryFn(data.hits);
+            ul.innerHTML = markup;
+        
+            const simplebox = new SimpleLightbox('.gallery a', {
+                captionsData: "alt", // встановлює підпис
+                captionDelay: 250, // встановлює затримку
+                doubleTapZoom: 0, // вбирає зум з подвійним кліком
+                maxZoom: 0, // вбирає зум
+                scrollZoom: false, //вбирає скрол мишою
+            });
+            simplebox.refresh()
+})
+        .catch(error => {
+            loader.style.display = 'none';
+            iziToast.error({
+                message: 'Error fetching images. Please try again later.',
+                position: 'topRight',
+              });
+              console.error(error);
+        })
         .finally(()=> {
             loader.classList.add('is-hidden');
+            loader.style.display = 'none';
         });
         form.reset();
 
-
-    loader.classList.remove('is-hidden');
 
 })
